92df280ba42828ca403e82c767f84a6217c895ad
Michal Ochman <ocherek@gmail.com>
2011-04-16 23:33:41 +0200

    Added jars to the lib

--- lib/quartz.rb
+++ lib/quartz.rb
@@ -5,6 +5,13 @@ $:.unshift(File.dirname(__FILE__)) unless
 require 'rubygems'
 require 'singleton'
 require 'active_support'
+
+# require jars                                                                                                                 
+require 'quartz/jars/commons-collections-3.2.1'                                                                                
+require 'quartz/jars/commons-logging'                                                                                          
+require 'quartz/jars/log4j'                                                                                                    
+require 'quartz/jars/quartz-1.6.4'
+
 require 'quartz/version'
 require 'quartz/job_factory'
 require 'quartz/job_detail'
